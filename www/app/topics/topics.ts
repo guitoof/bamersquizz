e4e9d26bb0fa98f0d7b3f3389b6d736b5c53a0e5
Guillaume Diallo-Mulliez <guillaume.diallo@ensta-paristech.fr>
2015-11-05 23:20:29 +0100

    Adding icons for topics and closes #1

--- www/app/topics/topics.ts
+++ www/app/topics/topics.ts
@@ -4,16 +4,18 @@ import {Page} from 'ionic/ionic';
   templateUrl: 'app/topics/topics.html'
 })
 export class Topics {
-
+  topics
   constructor() {
     this.topics  = [{
       id: 0,
       title: 'Angular 2',
-      description: 'Angular v2'
+      description: 'Angular v2',
+      icon: 'img/topics/angular2.png'
     }, {
       id: 1,
       title: 'Ionic 2',
-      description: 'ionic ... en mieux'
+      description: 'ionic en mieux',
+      icon: 'img/topics/ionic2.png'
     }];
   }
 
