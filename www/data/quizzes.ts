84f577826294c67e77687cf4900f0a00ae0b8fb2
Guillaume Diallo-Mulliez <guillaume.diallo@ensta-paristech.fr>
2015-11-16 02:17:58 +0100

    Change topics order

--- www/data/quizzes.ts
+++ www/data/quizzes.ts
@@ -1,33 +1,8 @@
 export default [
   {
-    title: 'Angular 2 Quizz',
-    icon: 'angular',
-    topicId: 0,
-    items: [{
-      question: 'What is the color of the angular logo ?',
-      choices: [
-        'green',
-        'red',
-        'blue',
-        'yellow'
-      ],
-      answer: 'red'
-    },
-    {
-      question: 'Who is the creator and maintainor of Angular 2 ?',
-      choices: [
-        'Facebook',
-        'Apple',
-        'IBM',
-        'Google'
-      ],
-      answer: 'Google'
-    }]
-  },
-  {
     title: 'Ionic 2 Quizz',
     icon: 'ionic',
-    topicId: 1,
+    topicId: 0,
     items: [{
       question: 'What is the color of the ionic logo ?',
       choices: [
@@ -47,6 +22,31 @@ export default [
         'Angular 2'
       ],
       answer: 'Angular 2'
+    }]
+  },
+  {
+    title: 'Angular 2 Quizz',
+    icon: 'angular',
+    topicId: 1,
+    items: [{
+      question: 'What is the color of the angular logo ?',
+      choices: [
+        'green',
+        'red',
+        'blue',
+        'yellow'
+      ],
+      answer: 'red'
+    },
+    {
+      question: 'Who is the creator and maintainor of Angular 2 ?',
+      choices: [
+        'Facebook',
+        'Apple',
+        'IBM',
+        'Google'
+      ],
+      answer: 'Google'
     }]
   },
   {
--- www/data/topics.ts
+++ www/data/topics.ts
@@ -1,14 +1,38 @@
 export default [
   {
     id: 0,
-    title: 'Angular 2',
-    description: 'Angular v2',
-    icon: 'img/topics/angular2.png',
+    title: 'Ionic 2',
+    description: 'Ionic... en mieux',
+    icon: 'img/topics/ionic2.png',
   },
   {
     id: 1,
-    title: 'Ionic 2',
-    description: 'ionic en mieux',
-    icon: 'img/topics/ionic2.png',
-  }
+    title: 'Angular 2',
+    description: 'Angular rethought, redesigned, released',
+    icon: 'img/topics/angular2.png',
+  },
+  {
+    id: 2,
+    title: 'Git',
+    description: 'The best version control system you can find',
+    icon: 'img/topics/git.png',
+  },
+  {
+    id: 3,
+    title: 'CSS 3',
+    description: 'Style your app',
+    icon: 'img/topics/css3.jpg',
+  },
+  {
+    id: 4,
+    title: 'Circle CI',
+    description: 'Continuous integration service',
+    icon: 'img/topics/circleci.png',
+  },
+  {
+    id: 5,
+    title: 'TypeScript',
+    description: 'Type awarness for Javascript',
+    icon: 'img/topics/typescript.png',
+  },
 ]
